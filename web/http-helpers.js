a9937c0a3b87351de63cf1a3a9e03b962012cda5
Michael Durfey <mdurfey@me.com>
2018-02-07 14:13:50 -0800

    init

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,10 +10,27 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
+exports.actions = {
+  //get to home page
+  'GET': function() {
+    console.log('get request');
+  },
+  'POST': function() {
+    console.log('post');
+  },
+  'PUT': function() {
+
+  },
+  'OPTIONS': function() {
+
+  }
+};
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,15 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  console.log(req);
+  const { method, url, headers, body} = req;
+  let action = httpHelpers.actions[method];
+  if (action) {
+    action();
+  }
+  console.log('VALUES!!!!', method, url, headers, body);
   res.end(archive.paths.list);
 };
